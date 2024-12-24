65885781619ac33c2a5313cdaff233a2617595b2
talhadaar <muhammadtalhadar@gmail.com>
2024-12-24 18:12:19 +0500

    use correct deployer and network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,10 +28,9 @@ const config: HardhatUserConfig = {
   defaultNetwork: "local",
   networks: {
     local: {
-      url: "http://127.0.0.1:8545",
+      url: "http://test1.peaq.network:10044",
       accounts: [
-        "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342",
-        "0xE2033D436CE0614ACC1EE15BD20428B066013F827A15CC78B063F83AC0BAAE64",
+        "0x6e26f3ed3e362f1edb359f77a83641787934a73426089f861a1a03d930feac5b",
       ],
     },
   },
